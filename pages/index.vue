62c379cbf2f4fabae0a496ae9b6cc039a3bfadc6
soonjing <39853992+soonjing@users.noreply.github.com>
2019-12-03 14:11:18 +0800

    Fixes

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,12 @@
 <template>
   <section class="container">
-    <div>
-      <h1 class="title">
-      </h1>
-    </div>
   </section>
 </template>
 
 <script>
 export default {
   mounted(){
-    this.$router.push("/play") ;
+    
   }
 };
 
